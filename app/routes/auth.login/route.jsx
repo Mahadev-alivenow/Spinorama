7bfcd16e1aa69bb523ce49110a12965a5ae184f3
ChallagidadMahadev <madhavan863@gmail.com>
2025-06-29 03:39:20 +0530

    auth1

--- app/routes/auth.login/route.jsx
+++ app/routes/auth.login/route.jsx
@@ -11,8 +11,8 @@ import {
 } from "@shopify/polaris";
 import polarisTranslations from "@shopify/polaris/locales/en.json";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
-import { login } from "../../shopify.server";
-import { loginErrorMessage } from "./error.server";
+// import { login } from "../../shopify.server";
+// import { loginErrorMessage } from "./error.server";
 import { redirect } from "@remix-run/node";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
