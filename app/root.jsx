e2fa6ea8c317599a7cb30d0735bd258404f4ca1e
ChallagidadMahadev <madhavan863@gmail.com>
2025-06-29 02:15:06 +0530

    updated host for shopify global

--- app/root.jsx
+++ app/root.jsx
@@ -122,10 +122,7 @@ export const loader = async ({ request }) => {
 export default function App() {
    const data = useLoaderData();
    const apiKey = data.apiKey || process.env.SHOPIFY_API_KEY || ""; 
-   const host =
-     typeof window !== "undefined"
-       ? new URLSearchParams(window.location.search).get("host")
-       : undefined || data.host || process.env.HOST || "";
+   const host = url.searchParams.get("host") || data.host || process.env.HOST || "";
 
 
       useEffect(() => {
